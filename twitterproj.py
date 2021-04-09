2be55493c82efb0a361c2b116853a62e27243116
kaamilmirza <kaamil.mirza.2002@gmail.com>
2021-04-09 11:37:57 +0530

    Update twitterproj.py
    Just a small project that I tried making when I was taking a python course. I explored using APIs in this one.

--- twitterproj.py
+++ twitterproj.py
@@ -1,7 +1,7 @@
-import tweepy
+import tweepy #please use pip install to get this library if you don't have or if you don't know how, search how to install external python libs
 import time
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret) #get consumer key, consumer secret, access token, and access token secret from twitter, google about it or watch some youtubue video
+auth.set_access_token(access_token, access_token_secret) #developers account/option enabled for twitter is must to get tokens it takes about 10 minutes to do it
 
 api = tweepy.API(auth)
 user = api.me()
@@ -13,13 +13,13 @@ def limit_handler(cursor):
 	except tweepy.RateLimitError:
 		time.sleep(300)
 
-search_string = ''
+search_string = '' #enter the string that you want to make the search for
 numberofTweets = 30
 
-for tweet in limit_handler(tweepy.Cursor(api.search, search_string).items(numberofTweets)):
+for tweet in limit_handler(tweepy.Cursor(api.search, search_string).items(numberofTweets)): #has limiter to make sure twitter doesn't think we spamming
 	try:
 		tweet.favorite()
-		print('I liked that tweet')
+		print('I liked that tweet') #just to print everything is going fine 
 	except tweepy.TweepError as e:
 		print(e.reason)
 	except StopIteration:
